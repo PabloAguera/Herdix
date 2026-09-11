fd92dd73e10b73c03357eb129f4dd6940d90454b
uo294557 <uo294557@uniovi.es>
2026-09-11 16:44:06 +0200

    cambio en los inicios de sesion

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     # JWT
     SECRET_KEY: str = ""
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 43200  # 30 días
 
     # Groq API
     GROQ_API_KEY: str = ""
